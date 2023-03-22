d8d510fdd954cbca40fc3d24dd36cf9035c95645
Prathm-s <gpkp168056@gmail.com>
2023-03-22 10:21:22 +0530

    Credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,15 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAeeuLD-EImwitl7jvKi7UTY3kwxzKaHRQ",
-  authDomain: "office-management-d728b.firebaseapp.com",
-  projectId: "office-management-d728b",
-  storageBucket: "office-management-d728b.appspot.com",
-  messagingSenderId: "904929700740",
-  appId: "1:904929700740:web:42542801ab087bc592cbd1"
-};
+import firebaseConfig from '../../credentials';
 
 
 initializeApp(firebaseConfig);
